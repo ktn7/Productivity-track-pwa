7cba43d21120127cce6a441c4e9b3a593fa56d10
ktn7 <ketanmorgaonkar4@gmail.com>
2025-08-10 16:23:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
 const CACHE_NAME = 'productivity-tracker-v1';
 const urlsToCache = [
     './', // This caches the root, and with start_url configured, it *should* load apptest.html
-    'apptest.html', // Make sure this is explicitly listed here!
+    'apptest1.html', // Make sure this is explicitly listed here!
     'manifest.json',
     'https://cdn.tailwindcss.com'
 ];
 // ... rest of your service worker code ...
+
